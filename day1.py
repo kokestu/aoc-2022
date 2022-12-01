c9e05d47950d547192806cc245643587ba199e88
Jonathan Sutton <jonny.a.sutton@gmail.com>
2022-12-01 21:03:18 +0000

    day 1

---
+++ day1.py
@@ -0,0 +1,26 @@
+# Part 1
+with open("inputs/day1-1.txt") as f:
+    data = f.read().splitlines()
+
+answer = 0
+current = 0
+for line in data:
+    if line == '':
+        answer = max(answer, current)
+        current = 0
+    else:
+        current += int(line)
+
+print(answer)
+
+# Part 2
+answers = []
+current = 0
+for line in data:
+    if line == '':
+        answers.append(current)
+        current = 0
+    else:
+        current += int(line)
+
+print(sum(sorted(answers)[-3:]))
